74f7f4f1a679b2163f8e38ffd389f2f11a761525
raveendra <raveendra@veda>
2015-12-24 13:35:48 -0500

    deploy bat file

--- raveweb/deploy.bat
+++ raveweb/deploy.bat
@@ -1,3 +1,3 @@
  
-copy  /b /y  target\raveweb.war  D:\RAVE\J\DEV\as\tom-jenkins\apache-tomcat-9.0.0.M1\webapps\raveweb.war
+copy  /b /y  raveweb\target\raveweb.war  D:\RAVE\J\DEV\as\tom-jenkins\apache-tomcat-9.0.0.M1\webapps\raveweb.war
  
